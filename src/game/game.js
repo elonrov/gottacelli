7f47127115b33bab3fb649d720a4bd5cd07e6ffb
Elon <elonrov@gmail.com>
2020-03-04 17:24:58 -0500

    nav bar with health and score; collision logic for oranges and clouds

--- src/game/game.js
+++ src/game/game.js
@@ -1,4 +1,6 @@
 import Cupid from './cupid';
+import Orange from './orange';
+import Cloud from './cloud';
 
 export default class Game {
     constructor(canvas) {
@@ -6,26 +8,183 @@ export default class Game {
         this.canvasWidth = canvas.width;
         this.canvasHeight = canvas.height;
         this.cupid = new Cupid(this.canvasWidth, this.canvasHeight);
+        
+        this.oranges = [];
+        let orange = new Orange;
+        this.oranges.push(orange);
 
-        this.play();
+        this.clouds = [new Cloud, new Cloud, new Cloud];
+        // let cloud = new Cloud;
+        // this.clouds.push(cloud);
+        
+        this.frameO = 0;
+        this.frameC = 0;        
 
+        this.frameId = null;
+
+        this.gameOver = this.gameOver.bind(this);
         this.gameUpdate = this.gameUpdate.bind(this);
-        this.gameUpdate();
+        this.restartGame = this.restartGame.bind(this);
+        this.endGame = this.endGame.bind(this);
+
+        this.detectOrangeCollision = this.detectOrangeCollision.bind(this);
+
+        const frame = document.getElementById("canvas-frame");
+        this.ctxFrame = frame.getContext("2d");
+        this.drawFrameNav = this.drawFrameNav.bind(this);
+        this.drawHealthBar = this.drawHealthBar.bind(this);
+        this.updateFrameNav = this.updateFrameNav.bind(this);
+
+        document.addEventListener("keydown", this.startHandler(), false);
     };
 
-    play() {
+    startHandler() {
+        const { restartGame } = this;
+        return e => {
+            e.preventDefault();
+            if (e.key == "Enter") {
+                console.log("enter");
+                restartGame();
+            }
+        }
+    };
+
+    endGame() {
+        document.getElementById("instructions").classList.add("visible");
+        this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+        this.cupid = new Cupid(this.canvasWidth, this.canvasHeight);
+        
         this.cupid.drawCupid(this.ctx);
     }
 
-    clear() {
-        this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+    restartGame() {
+        if (this.frameId) {
+            cancelAnimationFrame(this.frameId)
+        }
+        document.getElementById("instructions").classList.remove("visible");
+        // this.cupid = new Cupid(this.canvasWidth, this.canvasHeight);
+        this.cupid.drawCupid(this.cupid.ctx);
+        this.cupid.moveCupid("gravity");
+        // this.orange.drawOrange(this.orange.ctx);
+        // this.cloud.drawCloud(this.cloud.ctx);
+        // this.detectOrangeCollision();
+        
+        this.gameUpdate();
+        this.updateFrameNav();
+    }
+
+    gameOver() {
+        if ((this.cupid.cupidY + this.cupid.cupidHeight - 30 > this.canvasHeight) || this.cupid.health < 0) {
+            return true;
+        }
+        return false;
+    }
+
+    detectOrangeCollision(orange) {
+        const orangeCenterX = ((orange.orangeX + ( orange.orangeX +  orange.orangeWidth)) / 2 ); 
+        const orangeCenterY = ((orange.orangeY + ( orange.orangeY +  orange.orangeHeight)) / 2 );
+        const cupidTop = ((this.cupid.cupidY)); 
+        const cupidBottom = ((this.cupid.cupidY + this.cupid.cupidHeight));
+        const cupidLeft = ((this.cupid.cupidX)); 
+        const cupidRight = ((this.cupid.cupidX + this.cupid.cupidWidth));
+        if (((orangeCenterX > cupidLeft) && (orangeCenterX < cupidRight)) && 
+            ((orangeCenterY > cupidTop) && (orangeCenterY < cupidBottom))) {
+            // console.log("collision!")
+            this.oranges.shift();
+            this.cupid.score += 1;
+            return true;
+        } else if (orangeCenterY > this.canvasHeight) {
+            this.oranges.shift();
+        };
+    }
+
+    detectCloudCollision(cloud) {
+        const cloudCenterX = ((cloud.cloudX + (cloud.cloudX + cloud.cloudWidth)) / 2);
+        const cloudCenterY = ((cloud.cloudY + (cloud.cloudY + cloud.cloudHeight)) / 2);
+        const cupidTop = ((this.cupid.cupidY));
+        const cupidBottom = ((this.cupid.cupidY + this.cupid.cupidHeight));
+        const cupidLeft = ((this.cupid.cupidX));
+        const cupidRight = ((this.cupid.cupidX + this.cupid.cupidWidth));
+        if (((cloudCenterX > cupidLeft) && (cloudCenterX < cupidRight)) &&
+            ((cloudCenterY > cupidTop) && (cloudCenterY < cupidBottom))) {
+            // console.log("cloud collision!")
+            this.cupid.health -= 1.5;
+        }; 
+        if ((cloud.dir === "left") && (cloud.cloudX < -200)) {
+            this.clouds.shift(); 
+        };
     };
 
     gameUpdate() {
-        this.clear();
+        this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+
         this.cupid.newPos();
         this.cupid.drawCupid(this.ctx);
-        requestAnimationFrame(this.gameUpdate);
+        
+        this.frameO += 1;
+        this.frameC += 1;
+
+        this.oranges.forEach(orange => {
+            orange.drawOrange(this.ctx);
+        });
+
+        this.clouds.forEach(cloud => {
+            cloud.drawCloud(this.ctx);
+        })
+
+        if (this.frameO > 100) {
+            this.oranges.push(new Orange);
+            this.frameO = 0;
+        }; 
+
+        if(this.frameC > 200) {
+            this.clouds.push(new Cloud);
+            this.frameC = 0;
+        }; 
+
+        this.oranges.forEach(orange => {
+            this.detectOrangeCollision(orange);
+        });
+
+        this.clouds.forEach(cloud => {
+            this.detectCloudCollision(cloud);
+        });
+
+        if (this.gameOver()) {
+            console.log("Game Over! Play again.");
+            this.endGame();
+        }
+
+        // this.drawFrameNav()
+        this.frameId = requestAnimationFrame(this.gameUpdate);
+    }
+
+    drawFrameNav() {
+        this.ctxFrame.clearRect(0, 0, 900, 50);
+        this.ctxFrame.fillStyle = ("White");
+        this.ctxFrame.font = ("bolder 35px Arial");
+        this.ctxFrame.fillText(`Score: ${this.cupid.score} | Health: `, 15, 35)
+        this.ctxFrame.strokeText(`Score: ${this.cupid.score} | Health: `, 15, 35)
+    }
+
+    
+    drawHealthBar() {
+        this.ctxFrame.beginPath(); 
+        this.ctxFrame.rect(310, 10, 200, 25); 
+        this.ctxFrame.fillStyle = "red";
+        this.ctxFrame.fill();
+
+        this.ctxFrame.beginPath(); 
+        this.ctxFrame.rect(310, 10, this.cupid.health, 25); 
+        this.ctxFrame.fillStyle = "green"; 
+        this.ctxFrame.fill();
+    }
+    
+    updateFrameNav() {
+        this.drawFrameNav();
+        this.drawHealthBar();
+        requestAnimationFrame(this.updateFrameNav);
     }
 
 }
+
